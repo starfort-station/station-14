52350ce7725a360f78f783393756ac1570bf7b59
detf0r <ekimenkoip@gmail.com>
2023-07-25 20:31:34 +0800

    timedBehaviorsSystem fix

--- Content.Server/Spawners/EntitySystems/TimedBehaviorsSystem.cs
+++ Content.Server/Spawners/EntitySystems/TimedBehaviorsSystem.cs
@@ -1,18 +1,10 @@
 using Robust.Shared.Timing;
 using Content.Server.Spawners.Components;
 using Content.Server.Destructible;
-using Content.Server.Spawners.Components;
-using Robust.Shared.Timing;
-using Content.Shared.Stacks;
 using Content.Server.Stack;
 using System.Threading;
-using Content.Shared.Prototypes;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Random;
-using Robust.Shared.Serialization;
-using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype.List;
-using Robust.Shared.GameObjects;
-using System.Numerics;
 
 namespace Content.Server.Spawners.EntitySystems
 {
@@ -31,23 +23,6 @@ namespace Content.Server.Spawners.EntitySystems
 
             SubscribeLocalEvent<TimedBehaviorsComponent, ComponentStartup>(OnStartup);
             SubscribeLocalEvent<TimedBehaviorsComponent, TimerReached>(Exec);
-        }
-
-        private void Exec2(EntityUid owner, TimedBehaviorsComponent component, TimerCallback args)
-        {
-            var timeDif = _time.CurTime - component.LastActivationTime;
-            if (timeDif <= component.ActivationRate)
-                return;
-            component.LastActivationTime = _time.CurTime;
-            RaiseLocalEvent(owner, new TimerReached(), true);
-            foreach (var behavior in component.Behaviors)
-            {
-                // The owner has been deleted. We stop execution of behaviors here.
-                if (!EntityManager.EntityExists(owner))
-                    return;
-
-                behavior.Execute(owner, DestructibleSystem);
-            }
         }
 
         private void Exec(EntityUid owner, TimedBehaviorsComponent component, TimerReached args)
